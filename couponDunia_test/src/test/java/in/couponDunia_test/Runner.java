acabe7506a5cce98afb61d6b060253eeb8604b8b
vipin <vipin.sagar5@gmail.com>
2017-01-31 17:16:55 +0530

    add browser

---
+++ couponDunia_test/src/test/java/in/couponDunia_test/Runner.java
@@ -0,0 +1,14 @@
+package in.couponDunia_test;
+
+import in.couponDunia.utils.Driver_support;
+
+public class Runner extends Driver_support {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		initilalse_browser("https://help.github.com/articles/merging-a-pull-request/");
+
+	}
+
+}
